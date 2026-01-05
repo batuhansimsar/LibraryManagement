9169964cf510c4d9ae71fa32982b46db4da4a9c7
Eşref Batuhan Simsar <110763853+batuhansimsar@users.noreply.github.com>
2026-01-05 18:36:05 +0300

    Add Program.cs

---
+++ src/LibraryManagement.Web/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryManagement.Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LibraryManagement.Web.Data.LibraryContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+var app = builder.Build();
+
+// Auto-migrate database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<LibraryManagement.Web.Data.LibraryContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
